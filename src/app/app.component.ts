fe6eb642b45fb5424324b393f5d621a691fd9be7
AlexanderAliaga19 <u202417693@upc.edu.pe>
2026-04-16 23:26:50 -0500

    Add files via upload

---
+++ src/app/app.component.ts
@@ -0,0 +1,43 @@
+import { CommonModule } from '@angular/common';
+import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  isMenuOpen = false;
+
+  form = {
+    nombre: '',
+    email: '',
+    institucion: '',
+    mensaje: ''
+  };
+
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  closeMenu(): void {
+    this.isMenuOpen = false;
+  }
+
+  submitForm(): void {
+    console.log('Formulario enviado:', this.form);
+    alert('Gracias por tu mensaje. Nos pondremos en contacto pronto.');
+
+    this.form = {
+      nombre: '',
+      email: '',
+      institucion: '',
+      mensaje: ''
+    };
+
+    this.isMenuOpen = false;
+  }
+}
